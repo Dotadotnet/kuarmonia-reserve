a610e212c49530dc77cce8a3d69b3bf3a0b8496f
Amirali & Majid <160150114+Dotadotnet@users.noreply.github.com>
2025-04-18 13:36:55 +0330

    Update news sitemap.js

--- client/app/new/sitemap.js
+++ client/app/new/sitemap.js
@@ -1,7 +1,7 @@
 import Api from "@/utils/api"
 export default async function sitemap() {
   const sitemap = [];
-  const news = await Api('/new/get-news');
+  const news = await Api('/news/get-news');
   news.forEach(new_item => {
     sitemap.push({
       priority: 0.5,
