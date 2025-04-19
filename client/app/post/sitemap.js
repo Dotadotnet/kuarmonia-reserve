b46688ecc6be73d89b0a667b8191016e6ca80dab
Dotadotnet <Dotadotnet@outlook.com>
2025-04-19 21:01:02 +0330

    conflict sitemap post

--- client/app/post/sitemap.js
+++ client/app/post/sitemap.js
@@ -1,32 +1,32 @@
-// import Api from "@/utils/api"
-// export default async function sitemap() {
-//     const sitemap = [];
-//     const posts = await Api('/post/get-posts');
-//     posts.forEach(post => {
-//         if (post.featuredImage.type == "video") {
-//             sitemap.push({
-//                 priority: 0.4,
-//                 url: post.canonicalUrl,
-//                 lastModified: post.updatedDate,
-//                 changeFrequency: 'monthly',
-//                 images: [ process.env.HOST + '/banners/1.jpg' ],
-//                 videos: [
-//                     {
-//                         title: post.title,
-//                         thumbnail_loc: process.env.HOST + '/banners/1.jpg',
-//                         description: post.description,
-//                         content_loc: post.featuredImage.url,
-//                     },]
-//             })
-//         } else {
-//             sitemap.push({
-//                 priority: 0.4,
-//                 url: post.canonicalUrl,
-//                 lastModified: post.updatedDate,
-//                 changeFrequency: 'monthly',
-//                 images: [post.featuredImage.url],
-//             })
-//         }
-//     });
-//     return sitemap;
-// }
+import Api from "@/utils/api"
+export default async function sitemap() {
+    const sitemap = [];
+    const posts = await Api('/post/get-posts');
+    posts.forEach(post => {
+        if (post.featuredImage.type == "video") {
+            sitemap.push({
+                priority: 0.4,
+                url: post.canonicalUrl,
+                lastModified: post.updatedDate,
+                changeFrequency: 'monthly',
+                images: [ process.env.HOST + '/banners/1.jpg' ],
+                videos: [
+                    {
+                        title: post.title,
+                        thumbnail_loc: process.env.HOST + '/banners/1.jpg',
+                        description: post.description,
+                        content_loc: post.featuredImage.url,
+                    },]
+            })
+        } else {
+            sitemap.push({
+                priority: 0.4,
+                url: post.canonicalUrl,
+                lastModified: post.updatedDate,
+                changeFrequency: 'monthly',
+                images: [post.featuredImage.url],
+            })
+        }
+    });
+    return sitemap;
+}
