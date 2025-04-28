63d770dfa5a3222dffe95fc97b9d32fb7fcc1fda
Dotadotnet <Dotadotnet@outlook.com>
2025-04-28 19:59:51 +0330

    add  webp model

--- client/components/home/hero/Right.js
+++ client/components/home/hero/Right.js
@@ -50,14 +50,14 @@ function Right() {
             }}
           >
             <Image
-              src="/assets/home-page/banner/model1.png"
+              src="/assets/home-page/banner/model1.webp"
               alt="model"
               height={872}
               width={500}
               className="lg:w-[400px] ltr:-scale-x-100 hidden md:block lg:ml-0 md:ml-auto"
             />
             <Image
-              src="/assets/home-page/banner/model2.png"
+              src="/assets/home-page/banner/model2.webp"
               alt="model"
               height={872}
               width={500}
