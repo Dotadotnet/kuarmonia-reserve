3d614d0483fb0a3384d327e7b343dbd893f67a18
amirali <aminiamiraliamini1400@outlook.com>
2025-09-16 19:20:28 +0330

    slider visa edited

--- client/components/home/vias/VisaSlider.js
+++ client/components/home/vias/VisaSlider.js
@@ -12,6 +12,35 @@ const VisaSlider = ({ visa }) => {
   return (
     <Swiper
       className="w-full  px-4 !flex !justify-center"
+      autoplay={{
+        delay: 1000,
+        disableOnInteraction: true,
+      }}
+      loop={true}
+      speed={1300}
+      modules={[Autoplay]}
+      slidesPerView={1}
+      spaceBetween={10}
+      breakpoints={{
+        400: {
+          slidesPerView: 2,
+          spaceBetween: 10
+        },
+        // when window width is >= 480px
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        960: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        },
+        1280: {
+          slidesPerView: 4,
+          spaceBetween: 50
+        }
+      }}
     >
       {visa.map((visaItem) => (
         <SwiperSlide key={visaItem._id} className="!my-8">
