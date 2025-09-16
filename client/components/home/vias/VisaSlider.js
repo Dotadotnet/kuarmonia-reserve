cda8512a00d75da4a96dc4aa91f69975c1e37550
amirali <aminiamiraliamini1400@outlook.com>
2025-09-16 19:15:03 +0330

    visa cart color edite

--- client/components/home/vias/VisaSlider.js
+++ client/components/home/vias/VisaSlider.js
@@ -11,25 +11,6 @@ import "swiper/css/autoplay";
 const VisaSlider = ({ visa }) => {
   return (
     <Swiper
-      loop={true}
-      effect="coverflow"
-      grabCursor
-      centeredSlides
-      initialSlide={1}
-      slidesPerView="auto"
-      spaceBetween={80}
-      modules={[EffectCoverflow, Autoplay]}
-      autoplay={{
-        delay: 6000,
-        disableOnInteraction: false
-      }}
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows: false
-      }}
       className="w-full  px-4 !flex !justify-center"
       autoplay={{
         delay: 1000,
@@ -61,18 +42,13 @@ const VisaSlider = ({ visa }) => {
         }
       }}
     >
-      {visa && visa.length === 0
-        ? Array.from({ length: 8 }).map((_, index) => (
-            <SwiperSlide key={index} className="!w-fit ">
-              <VisaCardSkeleton />
-            </SwiperSlide>
-          ))
-        : visa.map((visaItem) => (
-            <SwiperSlide key={visaItem._id} className="!w-fit !my-8 !m-0">
-              <VisaCard visa={visaItem} />
-            </SwiperSlide>
-          ))}
-    </Swiper>
+      {visa.map((visaItem) => (
+        <SwiperSlide key={visaItem._id} className="!my-8">
+          <VisaCard visa={visaItem} />
+        </SwiperSlide>
+      ))
+      }
+    </Swiper >
   );
 };
 
