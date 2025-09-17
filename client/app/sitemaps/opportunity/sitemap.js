96789ace164f731d4b9d1505ae19b6d1609e0d64
amirali <aminiamiraliamini1400@outlook.com>
2025-09-17 15:15:51 +0330

    sitemap opportunity update

--- client/app/sitemaps/opportunity/sitemap.js
+++ client/app/sitemaps/opportunity/sitemap.js
@@ -2,15 +2,15 @@ import Api from "@/utils/api"
 import setLangSitemap from "@/utils/setLangSitemap";
 export default async function sitemap() {
     const sitemap = [];
-    const properties = await Api('/dynamic/get-all/property');
+    const opportunities = await Api('/dynamic/get-all/opportunity');
 
-    properties.forEach(property => {
+    opportunities.forEach(opportunity => {
         const item = {
             priority: 0.7,
-            url: process.env.NEXT_PUBLIC_BASE_URL + "/opportunity/" + property.propertyId + "/" +  property.slug ,
-            lastModified: property.lastUpdated,
+            url: process.env.NEXT_PUBLIC_BASE_URL + "/opportunity/" + opportunity.opportunityId + "/" +  opportunity.slug ,
+            lastModified: opportunity.lastUpdated,
             changeFrequency: 'weekly',
-            images : [property.thumbnail.url]
+            images : [opportunity.thumbnail.url]
         };
         sitemap.push(item)
     });    
